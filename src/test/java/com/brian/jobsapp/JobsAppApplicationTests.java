f73517cef4043683d4be18e5aef48816ee0d8953
Brian Njuguna <brian@ibuqacapital.com>
2024-06-24 23:28:08 +0300

    initialize project

---
+++ src/test/java/com/brian/jobsapp/JobsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.jobsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
