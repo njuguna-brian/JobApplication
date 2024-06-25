55457e04524754b43e2521ecc2329d88361c62d7
Brian Njuguna <brian@ibuqacapital.com>
2024-06-25 23:32:43 +0300

    adding Delete API endpoint

--- src/main/java/com/brian/jobsapp/job/JobController.java
+++ src/main/java/com/brian/jobsapp/job/JobController.java
@@ -34,4 +34,14 @@ public class JobController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id) {
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted) {
+            return new ResponseEntity<>("Job deleted successfully!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Job not found!", HttpStatus.NOT_FOUND);
+        }
+    }
 }
