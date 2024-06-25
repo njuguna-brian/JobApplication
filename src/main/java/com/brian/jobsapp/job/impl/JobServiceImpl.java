a1c3143c9ded1cd3fd6c75215232182335b4acc2
Brian Njuguna <brian@ibuqacapital.com>
2024-06-25 23:22:27 +0300

    adding ResponseEntity and HttpStatus

--- src/main/java/com/brian/jobsapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/brian/jobsapp/job/impl/JobServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Service
 public class JobServiceImpl implements JobService {
     private final List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Job> findAll() {
@@ -18,6 +19,17 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public void createJob(Job job) {
+        job.setId(nextId++);
         jobs.add(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for (Job job : jobs) {
+            if (job.getId().equals(id)) {
+                return job;
+            }
+        }
+        return null;
     }
 }
