de16c397fc769c332e788d4e8535d93e86a70519
Brian Njuguna <brian@ibuqacapital.com>
2024-06-26 23:46:37 +0300

    Persist data in the database

--- src/main/java/com/brian/jobsapp/job/Job.java
+++ src/main/java/com/brian/jobsapp/job/Job.java
@@ -1,12 +1,20 @@
 package com.brian.jobsapp.job;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private String minSalary;
     private String maxSalary;
     private String location;
+
+    public Job() {
+    }
 
     public Job(String minSalary, String maxSalary, String description, String title, Long id, String location) {
         this.minSalary = minSalary;
